83c0b354b0bd0ee05dcb0b2223706e3093dd6cac
Rene Klootwijk <rene.klootwijk@rencon.nl>
2021-04-02 22:35:00 +0200

    Fixed reference to description

--- test/01-parse_.spec.js
+++ test/01-parse_.spec.js
@@ -20,7 +20,7 @@ logger.add(new logger.transports.Console({
 describe('Check all different data types', function () {
   it('parse a bitmap data type (FLAG8)', function () {
     const result = parser('4000CCCC')
-    assert.strictEqual(result.msgType, 'Read-Ack', 'wrong message type reported')
+    assert.strictEqual(result.msgType.text, 'Read-Ack', 'wrong message type reported')
     assert.strictEqual(result.properties.fault, false, 'wrong property value reported')
     assert.strictEqual(result.properties.ch_active, false, 'wrong property value reported')
     assert.strictEqual(result.properties.dhw_active, true, 'wrong property value reported')
@@ -32,38 +32,38 @@ describe('Check all different data types', function () {
 
   it('Parse a float data type (F8_8)', function () {
     const result = parser('001B1490')
-    assert.strictEqual(result.msgType, 'Read-Data', 'wrong message type reported')
+    assert.strictEqual(result.msgType.text, 'Read-Data', 'wrong message type reported')
     assert.strictEqual(result.properties.outside_temperature, 20.56, 'wrong property value reported')
   })
 
   it('Parse a signed 8-bit integer data type (S8)', function () {
     const result = parser('10312230')
-    assert.strictEqual(result.msgType, 'Write-Data', 'wrong message type reported')
+    assert.strictEqual(result.msgType.text, 'Write-Data', 'wrong message type reported')
     assert.strictEqual(result.properties.ch_setpoint_high, 48, 'wrong property value reported')
     assert.strictEqual(result.properties.ch_setpoint_low, 34, 'wrong property value reported')
   })
 
   it('Parse a daytime data type (DAYTIME)', function () {
     const result = parser('2014541E')
-    assert.strictEqual(result.msgType, 'Invalid-Data', 'wrong message type reported')
+    assert.strictEqual(result.msgType.text, 'Invalid-Data', 'wrong message type reported')
     assert.strictEqual(result.properties.day_time, 'tuesday, 20:30', 'wrong property value reported')
   })
 
   it('Parse a date data type (DATE)', function () {
     const result = parser('3015030D')
-    assert.strictEqual(result.msgType, 'Reserved', 'wrong message type reported')
+    assert.strictEqual(result.msgType.text, 'Reserved', 'wrong message type reported')
     assert.strictEqual(result.properties.date, 'march, 13', 'wrong property value reported')
   })
 
   it('Parse a year data type (YEAR)', function () {
     const result = parser('401607E5')
-    assert.strictEqual(result.msgType, 'Read-Ack', 'wrong message type reported')
+    assert.strictEqual(result.msgType.text, 'Read-Ack', 'wrong message type reported')
     assert.strictEqual(result.properties.year, 2021, 'wrong property value reported')
   })
 
   it('Parse a remeha service data type (REMEHA_SERVICE)', function () {
     const result = parser('20840101')
-    assert.strictEqual(result.msgType, 'Invalid-Data', 'wrong message type reported')
+    assert.strictEqual(result.msgType.text, 'Invalid-Data', 'wrong message type reported')
     assert.strictEqual(result.properties.remeha_service_required, 'Type A', 'wrong property value reported')
   })
 })
